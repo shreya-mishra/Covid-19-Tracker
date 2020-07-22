4e2d9a46a789bc495a5fbe0f4757fdae1c9aee02
shreya-mishra <shreyamishra040398@gmail.com>
2020-07-23 02:15:06 +0530

    Updates

---
+++ precache-manifest.c919de6cdd538faf59b413638a21d6ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ba1c6f7ec65eb0ef8daba9555885bf",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "a6b44580074b9715cf15",
+    "url": "/Covid-19-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "a38aeb4a0d5310ac1cc0",
+    "url": "/Covid-19-Tracker/static/css/main.e9966181.chunk.css"
+  },
+  {
+    "revision": "a6b44580074b9715cf15",
+    "url": "/Covid-19-Tracker/static/js/2.3314c29a.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-19-Tracker/static/js/2.3314c29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38aeb4a0d5310ac1cc0",
+    "url": "/Covid-19-Tracker/static/js/main.e4ffc225.chunk.js"
+  },
+  {
+    "revision": "533a253de127af0c169c",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.4afae02b.js"
+  }
+]);
